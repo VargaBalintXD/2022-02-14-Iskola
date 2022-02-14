7cdb843965ead8d27fc1f7d16de4f8d8711f9aa1
VargaBalintXD <vargab.12d@acsjszki.hu>
2022-02-14 12:53:31 +0100

    gfgfg

--- 2022-02-14/Form1.Designer.cs
+++ 2022-02-14/Form1.Designer.cs
@@ -30,6 +30,8 @@ namespace _2022_02_14
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnTörlés = new System.Windows.Forms.Button();
+            this.btnMentés = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -41,11 +43,31 @@ namespace _2022_02_14
             this.listBox1.Size = new System.Drawing.Size(120, 94);
             this.listBox1.TabIndex = 0;
             // 
+            // btnTörlés
+            // 
+            this.btnTörlés.Location = new System.Drawing.Point(232, 205);
+            this.btnTörlés.Name = "btnTörlés";
+            this.btnTörlés.Size = new System.Drawing.Size(75, 23);
+            this.btnTörlés.TabIndex = 1;
+            this.btnTörlés.Text = "Törlés";
+            this.btnTörlés.UseVisualStyleBackColor = true;
+            // 
+            // btnMentés
+            // 
+            this.btnMentés.Location = new System.Drawing.Point(515, 233);
+            this.btnMentés.Name = "btnMentés";
+            this.btnMentés.Size = new System.Drawing.Size(75, 23);
+            this.btnMentés.TabIndex = 2;
+            this.btnMentés.Text = "Mentés";
+            this.btnMentés.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMentés);
+            this.Controls.Add(this.btnTörlés);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "IskolaGUI";
@@ -57,6 +79,8 @@ namespace _2022_02_14
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnTörlés;
+        private System.Windows.Forms.Button btnMentés;
     }
 }
 
